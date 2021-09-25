e4781d1c41d82aa36045864c376bb58a60b298d1
InimicalPart <37552123+InimicalPart@users.noreply.github.com>
2021-09-25 03:44:04 +0200

    Adding main page, year counter

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+const { text } = require('body-parser');
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
@@ -10,8 +11,37 @@ app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname + '/public/index.html'));
 })
 // GET /testing
-app.get('/testing', (req, res) => {
+/*app.get('/testing', (req, res) => {
 	res.sendFile(path.join(__dirname + '/public/testing.html'));
+})*/
+app.get("/api/yearsOld", function(req, res) {
+	res.setHeader('Content-Type', 'image/png');
+	time = (new Date() - new Date(1163622720000)) / (1000 * 60 * 60 * 24 * 365.2666666666666);
+	// console.log(time)
+	time = String(time).split(".")
+	time[1] = String(time[1]).padEnd(15, "0")
+	time = time.join(".")
+	// res.send(time)
+	const Jimp = require("jimp");
+	Jimp.read(path.join(__dirname, "public/img/blank.png")).then(function (image) {
+
+      Jimp.loadFont(Jimp.FONT_SANS_32_WHITE).then(function (font) {
+
+        image.print(font, 0,-5, time);
+        image.getBuffer(Jimp.MIME_PNG, (err, buffer) => {
+          image.write(path.join(__dirname, "public/img/text.png"));
+		  setTimeout(() => {
+		  res.sendFile(path.join(__dirname + '/public/img/text.png'));
+		  }, 1000);
+		})
+	}).then(function () {
+	});
+
+      }).catch(function (err) {
+        console.error(err);
+      });
+	  //send image to user
+
 })
 app.use(function (req, res, next) {
 	res.status(404).sendFile(path.join(__dirname + '/errors/404.html'));
---
+++ public/js/main.js
@@ -0,0 +1,241 @@
+/***********************************************
+ *  Copyright (c) 2021 InimicalPart
+ *  All rights reserved.
+ * 
+ *  This program is under the MIT license.
+ **********************************************/
+
+
+
+var dayConvert = 365.2666666666666; // the amount of days in a year (adjusted for my purpose)
+var mode = "seconds";
+function openTwtr() {
+  window.open("https://www.twitter.com/InimicalPart");
+}
+function openGH() {
+  window.open("https://github.com/InimicalPart");
+}
+function openDC() {
+  for (let i of document.getElementById("clickdc").parentNode.childNodes) {
+    if (i.tagName === "SPAN") {
+      i.classList.toggle("show");
+    }
+  }
+  copyTextToClipboard("InimicalPart ©#4542");
+}
+function openMail() {
+  window.location.href = "mailto:inimicalpart@gmail.com"; //TODO: change this to me@inimicalpart.com after inimicalpart.com is registered
+}
+function togglePopup(popup) {
+  popup.classList.toggle("show");
+}
+// prettier-ignore
+function copyTextToClipboard(text) {if (!navigator.clipboard) {fallbackCopyTextToClipboard(text);return;function fallbackCopyTextToClipboard(text){var textArea=document.createElement("textarea");textArea.value=text;textArea.style.top="0";textArea.style.left="0";textArea.style.position="fixed";document.body.appendChild(textArea);textArea.focus();textArea.select();try{document.execCommand("copy")}catch(err){}document.body.removeChild(textArea)}}navigator.clipboard.writeText(text);}
+function isRealEvent(e) {
+  // check if event is trusted
+  if (typeof e === "object" && e.originalEvent?.isTrusted) {
+    return true;
+  }
+  return false;
+}
+var modeWas = null;
+function setup() {
+  $("#pfpimg").on("dragstart", function (event) {
+    event.preventDefault();
+  });
+  setInterval(() => {
+    if (!document.hidden) {
+      // only execute if the page is visible to save resources
+      var modeIs = mode;
+      if (modeIs !== modeWas) {
+        try {
+          if (mode === "milliseconds") {
+            $("#willBeMode").html("milliseconds");
+          } else if (mode === "seconds") {
+            $("#willBeMode").text("seconds");
+          } else if (mode === "minutes") {
+            $("#willBeMode").text("minutes");
+          } else if (mode === "hours") {
+            $("#willBeMode").text("hours");
+          } else if (mode === "days") {
+            $("#willBeMode").text("days");
+          } else if (mode === "weeks") {
+            $("#willBeMode").text("weeks");
+          } else if (mode === "months") {
+            $("#willBeMode").text("months");
+          }
+        } catch (e) {
+          //do nothing, the user broke it.
+        }
+      }
+      modeWas = modeIs;
+    }
+  }, 50);
+  $("#willBeMode").click(function (e) {
+    if (!isRealEvent(e)) {
+      console.log("haha nah.");
+      return;
+    }
+    if (mode === "milliseconds") {
+      mode = "seconds";
+      $("#willBeMode").html("seconds");
+    } else if (mode === "seconds") {
+      mode = "minutes";
+      $("#willBeMode").text("minutes");
+    } else if (mode === "minutes") {
+      mode = "hours";
+      $("#willBeMode").text("hours");
+    } else if (mode === "hours") {
+      mode = "days";
+      $("#willBeMode").text("days");
+    } else if (mode === "days") {
+      mode = "weeks";
+      $("#willBeMode").text("weeks");
+    } else if (mode === "weeks") {
+      mode = "months";
+      $("#willBeMode").text("months");
+    } else if (mode === "months") {
+      mode = "milliseconds";
+      $("#willBeMode").text("milliseconds");
+    } else {
+      mode = "seconds";
+      $("#willBeMode").text("seconds");
+    }
+  });
+}
+setInterval(() => {
+  if (!document.hidden) {
+    // only execute if the page is visible to save resources
+    if (!$("#profileCircle").is(":hover")) {
+      $("#profileCircleName").css("opacity", "0");
+    }
+  }
+}, 50);
+setup();
+
+var unixAge = 1163622720000;
+let time;
+let timeLeftS;
+let timer = setInterval(() => {
+  if (!document.hidden) {
+    // only execute if the page is visible to save resources
+
+    let isYears = document.getElementById("isYears");
+    let willBeAge = document.getElementById("willBeAge");
+    let willBeAgeInSec = document.getElementById("willBeAgeInSec");
+    time =
+      (new Date() - new Date(unixAge)) /
+      (1000 * 60 * 60 * 24 * 365.2666666666666);
+    // console.log(time)
+    time = String(time).split(".");
+    time[1] = String(time[1]).padEnd(15, "0");
+    time = time.join(".");
+    isYears.innerHTML = "<b>" + time + "</b>";
+    const yearsNow = parseInt(time.toString().replace(/\..*/g, ""));
+    if (mode === "milliseconds") {
+      timeLeftS = nextBirthday(new Date(unixAge)) - new Date().getTime();
+    } else if (mode === "seconds") {
+      timeLeftS = "" + (nextBirthday(new Date(unixAge)) - new Date()) / 1000;
+      temp = timeLeftS.split(".");
+      temp[1] = String(temp[1]).substring(0, 3).padEnd(3, "0");
+      if (temp[1] === undefined) {
+        temp[1] = "0";
+      }
+      timeLeftS = temp.join(".");
+      // console.log(timeLeftS)
+    } else if (mode === "minutes") {
+      timeLeftS =
+        "" + (nextBirthday(new Date(unixAge)) - new Date()) / (1000 * 60);
+      temp = timeLeftS.split(".");
+      temp[1] = String(temp[1]).substring(0, 4).padEnd(4, "0");
+      if (temp[1] === undefined) {
+        temp[1] = "0";
+      }
+      timeLeftS = temp.join(".");
+    } else if (mode === "hours") {
+      timeLeftS =
+        "" + (nextBirthday(new Date(unixAge)) - new Date()) / (1000 * 60 * 60);
+      temp = timeLeftS.split(".");
+      temp[1] = String(temp[1]).substring(0, 5).padEnd(5, "0");
+      if (temp[1] === undefined) {
+        temp[1] = "0";
+      }
+      timeLeftS = temp.join(".");
+    } else if (mode === "days") {
+      timeLeftS =
+        "" +
+        (nextBirthday(new Date(unixAge)) - new Date()) / (1000 * 60 * 60 * 24);
+      temp = timeLeftS.split(".");
+      temp[1] = String(temp[1]).substring(0, 6).padEnd(6, "0");
+      if (temp[1] === undefined) {
+        temp[1] = "0";
+      }
+      timeLeftS = temp.join(".");
+    } else if (mode === "weeks") {
+      timeLeftS =
+        "" +
+        (nextBirthday(new Date(unixAge)) - new Date()) /
+          (1000 * 60 * 60 * 24 * 7);
+      temp = timeLeftS.split(".");
+      temp[1] = String(temp[1]).substring(0, 7).padEnd(7, "0");
+      if (temp[1] === undefined) {
+        temp[1] = "0";
+      }
+      timeLeftS = temp.join(".");
+    } else if (mode === "months") {
+      timeLeftS =
+        "" +
+        (nextBirthday(new Date(unixAge)) - new Date()) /
+          (1000 * 60 * 60 * 24 * 30);
+      temp = timeLeftS.split(".");
+      temp[1] = String(temp[1]).substring(0, 8).padEnd(8, "0");
+      if (temp[1] === undefined) {
+        temp[1] = "0";
+      }
+      timeLeftS = temp.join(".");
+    } else {
+      timeLeftS = "error";
+    }
+    if (timeLeftS !== "error") {
+      willBeAge.innerHTML = "<b>" + (yearsNow + 1) + "</b>";
+      willBeAgeInSec.innerHTML = "<b>" + timeLeftS + "</b>";
+      // document.getElementById("nextBDD").innerHTML = "<b>" + new Date(nextBirthday(new Date(unixAge))) + "</b>";
+    } else {
+      document.getElementById("iniUntil").innerHTML =
+        "<b>😢 y u break it man, i spend multiple hours on dis.</b>";
+    }
+  }
+}, 50);
+
+/**
+ *
+ * @param {bool} a @optional
+ */
+function nextBirthday(date, a) {
+  let thisYearBd = new Date(
+    date.getMonth() +
+      1 +
+      "/" +
+      date.getDate() +
+      "/" +
+      new Date().getFullYear() +
+      " " +
+      date.getHours() +
+      ":" +
+      date.getMinutes() +
+      ":" +
+      date.getSeconds()
+  );
+  // console.log(thisYearBd)
+  if (thisYearBd < new Date()) {
+    thisYearBd = thisYearBd.setFullYear(thisYearBd.getFullYear() + 1);
+  }
+  if (a) {
+    console.log(new Date(thisYearBd));
+  }
+  if (thisYearBd instanceof Date) {
+    return thisYearBd.getTime();
+  } else {
+    return thisYearBd;
+  }
+}
