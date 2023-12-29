3e8f2377b1728dcc99a936534e05774db158bb40
Inimi <me@inimicalpart.com>
2023-12-29 17:04:26 +0100

    Update notes.ts

--- backend_src/modules/notes.ts
+++ backend_src/modules/notes.ts
@@ -1,8 +1,21 @@
-import { Response, Request, Express } from "express-serve-static-core";
+import { Response, Request, Router } from "express-serve-static-core";
+import crypto, { CipherGCM, CipherGCMTypes, DecipherGCM } from 'node:crypto';
+import { MongoClient } from "mongodb";
+import { encrypt, decrypt, verify } from "../utils/encryption.js";
 
-// notes.inimicalpart.com, future project
-export async function main(API: Express) {
+import { config } from "dotenv";
 
+// log current working directory
+config({ path: "./dist/.env" });
+
+export async function main(API: Router) {
+
+    API.get(["/n/:id/:key", "/n/:id", "/"], async (req: Request, res: Response) => {
+        // send note.html
+        return res.sendFile("note.html", { root: "/var/www/notes" });
+
+
+    })
 }
 
 export default { main };
