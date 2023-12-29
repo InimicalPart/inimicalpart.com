c7945531dc4dce99ed564980c2f33a33c82ad574
Inimi <me@inimicalpart.com>
2023-12-29 16:59:07 +0100

    Create encryption.ts

---
+++ backend_src/utils/encryption.ts
@@ -0,0 +1,14 @@
+export function encrypt(plainText: string | object, password: any): string | undefined {
+  console.log("This function has been hidden from GitHub for security purposes.")
+ }
+ 
+export function verify(cipherText: string, password: any): boolean|undefined {
+  console.log("This function has been hidden from GitHub for security purposes.")
+}
+
+export function decrypt(cipherText: string, password: any): string | undefined {
+  console.log("This function has been hidden from GitHub for security purposes.")
+}
+
+
+export default { encrypt, decrypt, verify };
