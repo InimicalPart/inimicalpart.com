9bff558cf98710887a0f69d6c1968c77846dfe73
Inimi <me@inimicalpart.com>
2023-12-29 17:03:16 +0100

    Update extras.ts

--- backend_src/modules/extras.ts
+++ backend_src/modules/extras.ts
@@ -1,16 +1,16 @@
-import { Response, Request, Express } from "express-serve-static-core";
+import { Response, Request, Router } from "express-serve-static-core";
 import { GHK } from "../utils/github-keys.js";
 import { SEC } from "../utils/secrets.js";
 
-export async function main(API: Express) {
-    API.get(["/v1/getGHKey"], async (req: Request, res: Response) => {
+export async function main(API: Router) {
+    API.get(["/getGHKey"], async (req: Request, res: Response) => {
         try {
             await GHK(req, res);
         } catch (e) {
             console.log(e);
         }
     });
-    API.get(["/v1/getSecret"], async (req: Request, res: Response) => {
+    API.get(["/getSecret"], async (req: Request, res: Response) => {
         try {
             await SEC(req, res);
         } catch (e) {
