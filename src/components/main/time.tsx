3f627fc7ee7ee219bfcb433bbd5eeebb83f53821
Inimi <me@inimicalpart.com>
2024-06-07 03:30:42 +0200

    Added 24H support for contact page

--- src/components/main/time.tsx
+++ src/components/main/time.tsx
@@ -8,20 +8,28 @@ export default function TimeForInimi({
 }: {
     format?: string
 }) {
-    const [time, setTime] = useState("12:00:00 AM");
+    const [time, setTime] = useState("12:00:00?"); // "?" is a placeholder, to make it smoother when the time is loaded
     const [loading, setLoading] = useState(true);
+    const [useFormat, setUseFormat] = useState(format);
 
     useEffect(() => {
+        const locale = navigator.language
+        const wants12H = Intl.DateTimeFormat(locale,  { hour: 'numeric' }).resolvedOptions().hour12
+        if (wants12H) {
+            if (useFormat !== "h:mm:ss A") setUseFormat("h:mm:ss A")
+        } else {
+            if (useFormat !== "HH:mm:ss") setUseFormat("HH:mm:ss")
+        }
 
-        setTime(moment().tz("Europe/Stockholm").format(format));
-        setLoading(false);
+        setTime(moment().tz("Europe/Stockholm").format(useFormat));
+        if (loading) setLoading(false);
 
         const interval = setInterval(() => {
-            setTime(moment().tz("Europe/Stockholm").format(format));
+            setTime(moment().tz("Europe/Stockholm").format(useFormat));
         }, 500);
 
         return () => clearInterval(interval);
-    }, [format]);
+    },[useFormat, loading]);
 
     return <Skeleton className="mx-1 rounded-md" isLoaded={!loading}>{time}</Skeleton>
 
